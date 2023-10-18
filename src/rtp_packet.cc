44bfdddad72b1db69bf97c06061daa592924e3d8
liweijie <liweijie@offcn.com>
2023-10-18 14:35:12 +0800

    1 完成rtp包写入buffer逻辑 2 完成从buffer解析rtp包逻辑

--- src/rtp_packet.cc
+++ src/rtp_packet.cc
@@ -19,9 +19,12 @@
  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
  |          profile              |          length               |
  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ |                            ....                               |
+ |                            ....               |  padding size |
+ +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 */
 
-bool buffer2rtppacket(unsigned char *buffer, unsigned int size, rtppacket &pkt)
+bool read_rtppacket_from_buffer(unsigned char *buffer, unsigned int size, rtppacket &pkt)
 {
     unsigned char *current_pos = buffer;
 
@@ -64,12 +67,28 @@ bool buffer2rtppacket(unsigned char *buffer, unsigned int size, rtppacket &pkt)
 
         current_pos = current_pos + 2;
         pkt.extension_length = net2host_uint16(*((unsigned short *)current_pos));
+        pkt.extension_length_in_byte = pkt.extension_length * 4;
 
         unsigned char *extension_pos = current_pos + 2;
-        unsigned short extension_size = pkt.extension_length;
+        unsigned short extension_size = pkt.extension_length_in_byte;
+
+        unsigned short extension_idx = 0;
 
         //解析扩展头
 
+/*
+0                   1                   2                   3
+|7 6 5 4 3 2 1 0 7 6 5 4 3 2 1 0 7 6 5 4 3 2 1 0 7 6 5 4 3 2 1 0|
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+|       0xBE    |    0xDE       |           length=3            |
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+|  ID   | L=0   |     data      |  ID   |  L=1  |   data...     |
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+|     ...data   |    0 (pad)    |    0 (pad)    |  ID   | L=3   |
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+|                          data                                 |
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+*/
         while(extension_size > 0)
         {
             //取id
@@ -92,15 +111,17 @@ bool buffer2rtppacket(unsigned char *buffer, unsigned int size, rtppacket &pkt)
                 break;
             }
 
-            len = len + 1; //len 范围 [0, 15], 0 - 1字节，15 - 16字节
+            len = len + 1; //len 范围 [0, 15], 0 - 1字节，1 - 2字节，3 - 4字节，... ，15 - 16字节
 
-            pkt.extension_header[id - 1].id = id;
-            pkt.extension_header[id - 1].len = len;
+            pkt.extension_header[extension_idx].id = id; //id的范围为 1 - 14，
+            pkt.extension_header[extension_idx].len = len;
 
             extension_pos++;
             extension_size--;
+            extension_idx++;
+            pkt.extension_count++;
 
-            memcpy(pkt.extension_header[id - 1].data, extension_pos, len);
+            memcpy(pkt.extension_header[extension_idx].data, extension_pos, len);
 
             extension_pos = extension_pos + len;
             extension_size = extension_size - len;
@@ -112,6 +133,7 @@ bool buffer2rtppacket(unsigned char *buffer, unsigned int size, rtppacket &pkt)
 
     unsigned int rtp_data_size = current_pos - buffer;
 
+    //最后一个字节存放padding大小
     if (pkt.padding)
     {
         rtp_data_size = rtp_data_size - buffer[size - 1]; //不需要转换字节序？
@@ -123,14 +145,15 @@ bool buffer2rtppacket(unsigned char *buffer, unsigned int size, rtppacket &pkt)
 
     return true;
 }
-bool rtppacket2buffer(rtppacket &pkt, unsigned char *buffer, unsigned int &size)
+bool write_rtppacket_to_buffer(rtppacket &pkt, unsigned char *buffer, unsigned int &size)
 {
     memset(buffer, 0, size);
 
     unsigned short empty_size = size;
 
     unsigned char *current_pos = buffer;
-    *current_pos = (pkt.version << 6) | (pkt.padding << 5) | (pkt.x << 4) | (pkt.cc);
+    unsigned char extension_bit = (pkt.extension_count > 0) ? 1 : 0;
+    *current_pos = (pkt.version << 6) | (pkt.padding << 5) | extension_bit | (pkt.cc);
 
     current_pos++;
     *current_pos = (pkt.market << 7) | pkt.payload;
@@ -154,13 +177,44 @@ bool rtppacket2buffer(rtppacket &pkt, unsigned char *buffer, unsigned int &size)
         }
     }
 
-    if (pkt.x == 1)
+    if (pkt.extension_count > 0)
     {
+        //写profile - 0xBEDE
         (*(unsigned short *)current_pos) = host2net_uint16(pkt.extension_profile);
 
         current_pos = current_pos + 2;
+
+        unsigned short remainder = pkt.extension_length_in_byte % 4;
+        pkt.extension_length = pkt.extension_length_in_byte / 4 + remainder;
+
         (*(unsigned short *)current_pos) = host2net_uint16(pkt.extension_length);
 
+        //写真实的扩展信息
+        unsigned char *extension_pos = current_pos + 2;
+        unsigned short extension_size = 0;
+        for(int i = 0; i < pkt.extension_count; i++)
+        {
+            (*(unsigned char *)extension_pos) = (pkt.extension_header[i].id << 4) | (pkt.extension_header[i].len - 1);
+
+            memcpy(extension_pos, pkt.extension_header[i].data, pkt.extension_header[i].len);
+
+            extension_pos = extension_pos + 1 + pkt.extension_header[i].len;
+            extension_size = extension_size + 1 + pkt.extension_header[i].len;
+
+            //需要padding
+            if(remainder != 0 && i == (pkt.extension_count - 2))
+            {
+                //最后一个扩展数据长度能被4整除，则将padding数据放到其前面
+                if(pkt.extension_header[pkt.extension_count - 1].len % 4 == 0)
+                {
+                    memset(extension_pos, 0, remainder);
+                    extension_pos = extension_pos + remainder;
+                }
+            }
+        }
+
+        pkt.extension_length = extension_size / 4 + extension_size % 4;
+        
         current_pos = current_pos + 2 + pkt.extension_length * 4;
     }
 
@@ -173,5 +227,35 @@ bool rtppacket2buffer(rtppacket &pkt, unsigned char *buffer, unsigned int &size)
 
     memcpy(current_pos, pkt.data, pkt.data_len);
 
+    current_pos = current_pos + pkt.data_len;
+
+    if(pkt.padding_size > 0)
+    {
+        current_pos[pkt.padding_size - 1] = pkt.padding_size;
+        current_pos = current_pos + pkt.padding_size;
+    }
+
+    //返回包括padding在内 总数据大小
+    size = current_pos - buffer;
+
     return true;
+}
+
+void add_extension_header(struct rtppacket &pkt, struct rtp_extension_header &hdr)
+{
+    //如果已存在 直接返回
+    for(int i = 0; i < pkt.extension_count; i++)
+    {
+        if(pkt.extension_header[i].id == hdr.id)
+        {
+            return ;
+        }
+    }
+
+    pkt.extension_header[pkt.extension_count].id = hdr.id;
+    pkt.extension_header[pkt.extension_count].len = hdr.len;
+    memcpy(pkt.extension_header[pkt.extension_count].data, hdr.data, hdr.len);
+    
+    pkt.extension_count++;
+    pkt.extension_length_in_byte = pkt.extension_length_in_byte + hdr.len;
 }
--- src/rtp_packet.h
+++ src/rtp_packet.h
@@ -3,14 +3,15 @@
 
 #include "base_types.h"
 
-const unsigned char kRtpVersion = 2;
-const unsigned short kOneByteHeaderId = 0xBEDE;
+const unsigned char  kRtpVersion = 2;   //rtp版本号固定位为2
+const unsigned short kOneByteHeaderId = 0xBEDE; //webrtc只支持这一种
+const unsigned short kExtensionCount = 16;
 
 //只支持one-byte-header
 struct rtp_extension_header
 {
     unsigned char id; //范围[1, 14]
-    unsigned char len;//范围[0, 15]
+    unsigned char len;//rfc中范围[0, 15]，这里我们使用真实占用的字节数，即范围为 [1, 16]
     unsigned char data[16];
 };
 
@@ -18,7 +19,7 @@ struct rtppacket
 {
     unsigned char version;
     unsigned char padding;
-    unsigned char x;
+    unsigned char x;    //1 - 有扩展头
     unsigned char cc;
     unsigned char market;
     unsigned char payload;
@@ -31,9 +32,12 @@ struct rtppacket
     unsigned int csrc[16];
 
     //扩展头信息
-    unsigned short extension_profile;
-    unsigned short extension_length;
-    struct rtp_extension_header extension_header[14];
+    unsigned short extension_profile; //0xBEDE
+    unsigned short extension_length; //有多少个4字节数据
+    unsigned short extension_length_in_byte;
+    unsigned short extension_count;
+
+    struct rtp_extension_header extension_header[kExtensionCount];
 
     unsigned short padding_size;
 
@@ -41,7 +45,9 @@ struct rtppacket
     unsigned short data_len;
 };
 
-bool buffer2rtppacket(unsigned char *buffer, unsigned int size, rtppacket &pkt);
-bool rtppacket2buffer(rtppacket &pkt, unsigned char *buffer, unsigned int &size);
+bool read_rtppacket_from_buffer(unsigned char *buffer, unsigned int size, rtppacket &pkt);
+bool write_rtppacket_to_buffer(rtppacket &pkt, unsigned char *buffer, unsigned int &size);
+
+void add_extension_header(struct rtppacket &pkt, struct rtp_extension_header &hdr);
 
 #endif //#ifndef __rtp_defines_h__
